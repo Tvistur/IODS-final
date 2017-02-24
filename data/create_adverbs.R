13fca5f880ed5ff1bbfad3229e4a5588028d8d29
Tvistur <susan.huotari@helsinki.fi>
2017-02-24 19:01:09 +0200

    spell check

--- data/create_adverbs.R
+++ data/create_adverbs.R
@@ -1,10 +1,12 @@
 # Susan Huotari
 # susan.huotari@helsinki.fi
 # 24.2.2017
+
 # Wrangling for IODS-final
-## The data was collected from the Old Bailey Corpus where adverb-adjective pairs were searched for.
-## Then the results were copied into Excel where they were categorized according to the type of adverb used (derived/other)
-## and whether the adverb functions as an intesifier.
+
+## The data was originally collected from the Old Bailey Corpus where adverb-adjective pairs were searched for. Then the results were copied into Excel where they were refined and categorized.
+
+## In this wranglig scrip I select a group of variables, filter out missing values from them, modify variable names and value labels as well as save the results into a new file for further analysis.
 
 
 # IMPORT DATA
@@ -13,12 +15,10 @@ adverbs <- read.csv("/Users/susanhuotari/Desktop/adverbs.csv", sep = ";", header
 summary(adverbs)
 # the data consists of a total of 23 variables & 7,828 observations...
 
-# The data contains a lot of metadata that is not (or no longer) relevant for the forthcoming study
-# Also, NA's in both age' and 'age group' is over 7,000 and should I include them and then remove 
-# NA's from the data set, there would be very little data to work with - so I will not include these variables.
+# The data contains a lot of metadata that is not (or no longer) relevant for the forthcoming study. Also, NA's in both age' and 'age group' is over 7,000 and should I include them and then remove NA's from the data set, there would be very little data to work with - so I will not include these variables.
 
 
-# SELECT INDEPENDENT VARIABLES TO KEEP
+# SELECT VARIABLES TO KEEP
 keep <- c("adverb_type", "intensifier", "part1", "part2", "Year", "Sex", "Class", "Role")
 adverbs_new <- select(adverbs, one_of(keep))
 
@@ -39,7 +39,7 @@ colnames(adverbs_new)[6] <- "gender"
 colnames(adverbs_new)[7] <- "class"
 colnames(adverbs_new)[8] <- "role"
 
-# MODIFY VARIABLE VALUES TO MAKE THEM EASIER TO WORK WITH
+# MODIFY VARIABLE VALUE LABELS TO MAKE THEM EASIER TO WORK WITH
 
 # variable: adverb_type
 sort(levels(adverbs_new$adverb_type))
